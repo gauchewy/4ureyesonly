964fc8be6a0b3ee4da2482d8d78ffbac2cbf4e64
gauchewy <ql@qiweis-mbp.lan>
2022-09-23 22:04:29 -0400

    initial commit

---
+++ 4ureyesonly/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  4ureyesonly
+//
+//  Created by Qiwei on 9/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 4ureyesonly/_ureyesonlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ureyesonlyApp.swift
+//  4ureyesonly
+//
+//  Created by Qiwei on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct _ureyesonlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
